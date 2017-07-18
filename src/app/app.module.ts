1026d56248d53e28d55fdfdf97b707c15a08a057
Taras Emelyanenko <php.laboratory@gmail.com>
2017-07-18 12:35:35 +0300

    maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+//import { HomePage } from '../pages/home/home';
+import { TagPage } from '../pages/tag/tag';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    TagPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +20,7 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    TagPage
   ],
   providers: [
     StatusBar,
